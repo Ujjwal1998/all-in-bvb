6d1869d6da934b8196c975fc98a264d10f7aad48
Ujjwal Talwar (utalwar) <utalwar@cisco.com>
2024-08-06 20:48:22 +0530

    removed vote acc body

--- bvb09-ui/src/components/voteaccordion.jsx
+++ bvb09-ui/src/components/voteaccordion.jsx
@@ -12,50 +12,50 @@ function VoteAccordion({
   setHasVoted,
   voteData,
 }) {
-  const startingEleven = children[0].startXI;
-  const fixture = selectedMatch.fixture;
-  const league = selectedMatch.league;
-  const subDetails = selectedMatch.events.filter(
-    (evt) =>
-      evt.type == "subst" &&
-      evt.team.id == import.meta.env.VITE_APIFOOTBALL_TEAM_ID
-  );
-  const subs = subDetails.map((sub) => sub.assist);
-  const voteClickHandler = async (playerID) => {
-    const payload = {
-      player: {
-        id: playerID,
-      },
-      fixture: {
-        id: fixture.id,
-        date: fixture.date,
-        league: league,
-      },
-    };
-    const voteRequest = await axios.post(
-      `${import.meta.env.VITE_NODE_HOST}/api/players/vote`,
-      payload
-    );
-    if (voteRequest) {
-      let voteData = JSON.parse(localStorage.getItem("hasVoted"));
-      console.log(voteData);
-      // ? whats the point of this, to check if has already voted ? but i am pushing data here
-      if (voteData) {
-        let obj = {};
-        obj[fixture.id] = true;
-        voteData.push(obj);
-        localStorage.setItem("hasVoted", JSON.stringify(voteData));
-      } else {
-        voteData = [];
-        let obj = {};
-        obj[fixture.id] = true;
-        voteData.push(obj);
-        localStorage.setItem("hasVoted", JSON.stringify(voteData));
-      }
-      setHasVoted(true);
-      return voteRequest;
-    }
-  };
+  // const startingEleven = children[0].startXI;
+  // const fixture = selectedMatch.fixture;
+  // const league = selectedMatch.league;
+  // const subDetails = selectedMatch.events.filter(
+  //   (evt) =>
+  //     evt.type == "subst" &&
+  //     evt.team.id == import.meta.env.VITE_APIFOOTBALL_TEAM_ID
+  // );
+  // const subs = subDetails.map((sub) => sub.assist);
+  // const voteClickHandler = async (playerID) => {
+  //   const payload = {
+  //     player: {
+  //       id: playerID,
+  //     },
+  //     fixture: {
+  //       id: fixture.id,
+  //       date: fixture.date,
+  //       league: league,
+  //     },
+  //   };
+  //   const voteRequest = await axios.post(
+  //     `${import.meta.env.VITE_NODE_HOST}/api/players/vote`,
+  //     payload
+  //   );
+  //   if (voteRequest) {
+  //     let voteData = JSON.parse(localStorage.getItem("hasVoted"));
+  //     console.log(voteData);
+  //     // ? whats the point of this, to check if has already voted ? but i am pushing data here
+  //     if (voteData) {
+  //       let obj = {};
+  //       obj[fixture.id] = true;
+  //       voteData.push(obj);
+  //       localStorage.setItem("hasVoted", JSON.stringify(voteData));
+  //     } else {
+  //       voteData = [];
+  //       let obj = {};
+  //       obj[fixture.id] = true;
+  //       voteData.push(obj);
+  //       localStorage.setItem("hasVoted", JSON.stringify(voteData));
+  //     }
+  //     setHasVoted(true);
+  //     return voteRequest;
+  //   }
+  // };
   console.log(hasVoted, "hasVoted");
   return (
     <div className="text-white">
